35034ebe2504c70dc5ab4b24fcecc9cae7c96b89
nbt311 <nbtrung0311@gmail.com>
2024-01-29 10:38:16 +0700

    add deme show list of fruits

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {findAllByDisplayValue} from "@testing-library/react";
 const name = "Your name";
 React.createElement("h1",{style: {textAlign: "center"}},name);
+const fruits = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Apricot",
+    "Black rowan",
+    "Cranberry"];
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    React.createElement("h1",{style: {textAlign: "center"}},name)
+    // React.createElement("h1",{style: {textAlign: "center"}},name)
+    <div>
+        <h1>List of Fruits</h1>
+        <ul>
+            {fruits.map(item => (
+                <li>{item}</li>
+            ))}
+        </ul>
+    </div>
 );
