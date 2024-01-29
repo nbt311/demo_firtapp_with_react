137271251db6079eebf2157a82c21c50de69066f
nbt311 <nbtrung0311@gmail.com>
2024-01-29 10:52:49 +0700

    show Local Time

--- src/index.js
+++ src/index.js
@@ -11,14 +11,22 @@ const fruits = [
     "Black rowan",
     "Cranberry"];
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    // React.createElement("h1",{style: {textAlign: "center"}},name)
-    <div>
-        <h1>List of Fruits</h1>
-        <ul>
-            {fruits.map(item => (
-                <li>{item}</li>
-            ))}
-        </ul>
-    </div>
-);
+const tick = () => {
+    root.render(
+        // React.createElement("h1",{style: {textAlign: "center"}},name)
+        <div>
+            <h1>{name}</h1>
+            <h1>List of Fruits</h1>
+            <ul>
+                {fruits.map(item => (
+                    <li>{item}</li>
+                ))}
+            </ul>
+            <h1>
+                Hello,world!
+            </h1>
+            <h2>It is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
+};
+setInterval(tick, 1000);
